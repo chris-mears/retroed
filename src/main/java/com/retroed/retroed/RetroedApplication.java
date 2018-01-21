b5e74e639954b1bc52960f19b192b0fe6d923705
SadEvilPenguin <mears.chris@gmail.com>
2018-01-21 14:11:52 -0500

    Initial Spring Boot application setup

---
+++ src/main/java/com/retroed/retroed/RetroedApplication.java
@@ -0,0 +1,12 @@
+package com.retroed.retroed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroedApplication.class, args);
+	}
+}
---
+++ src/test/java/com/retroed/retroed/RetroedApplicationTests.java
@@ -0,0 +1,16 @@
+package com.retroed.retroed;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RetroedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
